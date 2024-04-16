9b54811113defdb782fa23825b4f6951d74ac85f
Tina Engel <tina.engel@mendix.com>
2024-04-16 15:33:10 +0000

    SAS-380 java action can set function toolchoice in request

---
+++ javasource/openaiconnector/impl/ChatCompletionsMessageRequestImpl.java
@@ -0,0 +1,27 @@
+package openaiconnector.impl;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.mendix.core.Core;
+
+import openaiconnector.proxies.ChatCompletionsMessages;
+import openaiconnector.proxies.ChatCompletionsMessageRequest;
+import openaiconnector.proxies.ENUM_Role;
+import com.mendix.systemwideinterfaces.core.IContext;
+
+public class ChatCompletionsMessageRequestImpl {
+	
+	public static List<ChatCompletionsMessageRequest> retrieveMessageListByRole (ChatCompletionsMessages chatCompletionsMessages, ENUM_Role filterByRole, IContext context){
+	
+		List<ChatCompletionsMessageRequest> messageList = Core.retrieveByPath(context, chatCompletionsMessages.getMendixObject(), ChatCompletionsMessageRequest.MemberNames.ChatCompletionsMessageRequest_ChatCompletionsMessages.toString())
+				.stream()
+				.filter(mxObject -> {
+					 String messageRole = mxObject.getValue(context, ChatCompletionsMessageRequest.MemberNames.Role.name());
+				     return messageRole.equals(filterByRole.name());
+				})
+				.map(mxObject -> ChatCompletionsMessageRequest.initialize(context, mxObject))
+				.collect(Collectors.toList());
+		return messageList;
+	}	
+}
