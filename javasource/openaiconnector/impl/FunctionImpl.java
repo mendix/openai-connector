81858439b2c2be730376c05d3c939959c2aee4d1
Tina Engel <tina.engel@mendix.com>
2024-04-24 21:00:43 +0000

    SAS-380 removed Arguments import mapping with is now handled in the ExecuteFunctionMicroflow Java action

--- javasource/openaiconnector/impl/FunctionImpl.java
+++ javasource/openaiconnector/impl/FunctionImpl.java
@@ -2,29 +2,28 @@ package openaiconnector.impl;
 
 import java.util.Map;
 import java.util.Set;
-
 import com.mendix.core.Core;
 import com.mendix.systemwideinterfaces.core.IDataType;
 
 public class FunctionImpl {
 	
-	public static boolean validateFunctionMicroflow(String actionMicroflow) {
+	public static boolean validateFunctionMicroflow(String FunctionMicroflow) {
 		Set<String> microflowNames = Core.getMicroflowNames();
-		if(!microflowNames.contains(actionMicroflow)) {
-			throw new IllegalArgumentException("ActionMicroflow " + actionMicroflow + " does not exists.");
+		if(!microflowNames.contains(FunctionMicroflow)) {
+			throw new IllegalArgumentException("FunctionMicroflow " + FunctionMicroflow + " does not exists.");
 		}
 		
-		Map<String, IDataType> inputParameters = Core.getInputParameters(actionMicroflow);
+		Map<String, IDataType> inputParameters = Core.getInputParameters(FunctionMicroflow);
 		if (inputParameters == null || inputParameters.size() != 1) {
-			throw new IllegalArgumentException("ActionMicroflow " + actionMicroflow + " should only have one input parameter of type String.");
+			throw new IllegalArgumentException("FunctionMicroflow " + FunctionMicroflow + " should only have one input parameter of type String.");
 		}
 		
 		if(IDataType.DataTypeEnum.String.equals(inputParameters.entrySet().iterator().next().getValue().getType()) == false) {
-			throw new IllegalArgumentException("ActionMicroflow " + actionMicroflow + " should have an input parameter of type String.");			
+			throw new IllegalArgumentException("FunctionMicroflow " + FunctionMicroflow + " should have an input parameter of type String.");			
 		}
 
-		if(Core.getReturnType(actionMicroflow) == null || IDataType.DataTypeEnum.String.equals(Core.getReturnType(actionMicroflow).getType()) == false) {
-			throw new IllegalArgumentException("ActionMicroflow " + actionMicroflow + " should have a String return value.");		
+		if(Core.getReturnType(FunctionMicroflow) == null || IDataType.DataTypeEnum.String.equals(Core.getReturnType(FunctionMicroflow).getType()) == false) {
+			throw new IllegalArgumentException("FunctionMicroflow " + FunctionMicroflow + " should have a String return value.");		
 		}
 		return true;
 	}
