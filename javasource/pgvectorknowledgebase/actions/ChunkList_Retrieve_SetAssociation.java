8b4e9a4f3ff4a851531cfa665252805ff4b678be
Tom Warmerdam <tom.warmerdam@mendix.com>
2024-05-08 11:16:54 +0000

    SAS-378: java fix

--- javasource/pgvectorknowledgebase/actions/ChunkList_Retrieve_SetAssociation.java
+++ javasource/pgvectorknowledgebase/actions/ChunkList_Retrieve_SetAssociation.java
@@ -93,6 +93,6 @@ public class ChunkList_Retrieve_SetAssociation extends CustomJavaAction<java.uti
 	}
 
 	// BEGIN EXTRA CODE
-	private static final MxLogger LOGGER = new MxLogger(ChunkList_RetrieveNearestNeighbors_SetAssociation.class);
+	private static final MxLogger LOGGER = new MxLogger(ChunkList_Retrieve_SetAssociation.class);
 	// END EXTRA CODE
 }
