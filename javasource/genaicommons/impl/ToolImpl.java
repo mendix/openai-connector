653d51aa2b101381ba1da382cbba2bfcf30cc26f
Tom Warmerdam <tom.warmerdam@mendix.com>
2024-05-23 09:45:55 +0000

    SAS-473: fix java for toolcollection helpers

--- javasource/genaicommons/impl/ToolImpl.java
+++ javasource/genaicommons/impl/ToolImpl.java
@@ -1,81 +1,19 @@
 package genaicommons.impl;
 
-import static java.util.Objects.requireNonNull;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.regex.Pattern;
 
-import com.mendix.core.Core;
-import com.mendix.core.CoreException;
-import com.mendix.systemwideinterfaces.core.IContext;
-import com.mendix.systemwideinterfaces.core.IDataType;
-
-import genaicommons.proxies.Function;
-import genaicommons.proxies.Tool;
-import genaicommons.proxies.ToolCollection;
-
-
-
 public class ToolImpl {
-	
-	public static void validateInput(String functionMicroflow, String functionName) throws Exception {
-		requireNonNull(functionMicroflow, "FunctionMicroflow is required.");
-		requireNonNull(functionName, "FunctionName is required.");
-		validateFunctionName(functionName);
-		validateFunctionMicroflow(functionMicroflow);
-	}
 		
-	private static void validateFunctionName(String functionName) throws Exception {
+	protected static void validateToolName(String toolName) throws Exception {
 	    // Name must be a-z, A-Z, 0-9, or contain underscores and dashes, with a maximum length of 64
 	    String pattern = "^[a-zA-Z0-9_-]{1,64}$";
 	    Pattern regex = Pattern.compile(pattern);
 
 	    // Check if the input string matches the pattern
-	    if(!regex.matcher(functionName).matches()) {
+	    if(!regex.matcher(toolName).matches()) {
 	    	throw new IllegalArgumentException("Function Name is not valid. Name must be a-z, A-Z, 0-9, or contain underscores and dashes, with a maximum length of 64.");
 	    }
 	}
 	
-	public static void validateFunctionMicroflow(String functionMicroflow) throws Exception {
-		Set<String> microflowNames = Core.getMicroflowNames();
-		if(!microflowNames.contains(functionMicroflow)) {
-			throw new IllegalArgumentException("FunctionMicroflow " + functionMicroflow + " does not exists.");
-		}
-		
-		Map<String, IDataType> inputParameters = Core.getInputParameters(functionMicroflow);
-		if (inputParameters != null && inputParameters.size() > 1) {
-			throw new IllegalArgumentException("FunctionMicroflow " + functionMicroflow + " should only have one input parameter of type String.");
-		}
-		
-		if(inputParameters != null && !inputParameters.entrySet().isEmpty()
-				&& IDataType.DataTypeEnum.String.equals(inputParameters.entrySet().iterator().next().getValue().getType()) == false) {
-			throw new IllegalArgumentException("FunctionMicroflow " + functionMicroflow + " should have an input parameter of type String.");			
-		}
-
-		if(Core.getReturnType(functionMicroflow) == null || IDataType.DataTypeEnum.String.equals(Core.getReturnType(functionMicroflow).getType()) == false) {
-			throw new IllegalArgumentException("FunctionMicroflow " + functionMicroflow + " should have a String return value.");		
-		}
-	}
 	
-	public static Tool createTool(IContext context, String functionMicroflow, String functionName, String functionDescription, ToolCollection toolCollection) throws CoreException {
-		Function tool = new Function(context);
-		tool.setMicroflow(functionMicroflow);
-		tool.setName(functionName);	
-		tool.setDescription(functionDescription); //Optional parameter
-		List<Tool> ToolList = toolCollection.getToolCollection_Tool();
-		ToolList.add(tool);
-		toolCollection.setToolCollection_Tool(ToolList); 
-		return tool;
-	}
-	
-	public static String getFirstInputParamName(String functionMicroflow) {
-		Map<String, IDataType> inputParameters = Core.getInputParameters(functionMicroflow);
-		if(inputParameters != null && !inputParameters.entrySet().isEmpty()) {
-			return inputParameters.entrySet().iterator().next().getKey();
-		} else {
-			return null;
-		}
-	}
 }
