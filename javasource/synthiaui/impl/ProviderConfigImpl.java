1b77d1705a0118c8a03fc0dcb96fe265fef65f6e
Tom Warmerdam <tom.warmerdam@mendix.com>
2024-05-28 06:43:45 +0000

    SAS-475: import SynthiaUI and merge necessary examples part 1

---
+++ javasource/synthiaui/impl/ProviderConfigImpl.java
@@ -0,0 +1,28 @@
+package synthiaui.impl;
+
+import java.util.Map;
+import com.mendix.core.Core;
+import com.mendix.systemwideinterfaces.core.IDataType;
+import synthiaui.proxies.ChatContext;
+
+public class ProviderConfigImpl{
+	
+	public static void validate(String ActionMicroflow) {
+		if (ActionMicroflow == null || ActionMicroflow.isBlank()) {
+			throw new IllegalArgumentException("ActionMicroflow is required.");
+		}
+		
+		Map<String, IDataType> inputParameters = Core.getInputParameters(ActionMicroflow);
+		if (inputParameters == null || inputParameters.size() != 1) {
+			throw new IllegalArgumentException("ActionMicroflow " + ActionMicroflow + " should only have one input parameter of type " + ChatContext.getType() + ".");
+		}
+		
+		if(ChatContext.getType().equals(inputParameters.entrySet().iterator().next().getValue().getObjectType()) == false) {
+			throw new IllegalArgumentException("ActionMicroflow " + ActionMicroflow + " should have an input parameter of type " + ChatContext.getType()+ ".");			
+		}
+
+		if(Core.getReturnType(ActionMicroflow) == null || IDataType.DataTypeEnum.Boolean.equals(Core.getReturnType(ActionMicroflow).getType()) == false) {
+			throw new IllegalArgumentException("ActionMicroflow " + ActionMicroflow + " should have a Boolean return value.");		
+		}
+	}	
+}
