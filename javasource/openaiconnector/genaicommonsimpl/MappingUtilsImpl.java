652b8a9a91bdcfb72a3373d3c237b4b511fa0a9e
Tom Warmerdam <tom.warmerdam@mendix.com>
2024-06-12 20:11:31 +0000

    SAS-511: include MappingUtils.java in OpenAIconnector and change JA to use it

---
+++ javasource/openaiconnector/genaicommonsimpl/MappingUtilsImpl.java
@@ -0,0 +1,34 @@
+package openaiconnector.genaicommonsimpl;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+public class MappingUtilsImpl {
+	
+	public static ObjectNode createFunctionParametersNode(String functionMicroflow, ObjectMapper MAPPER) {
+		String inputParamName = FunctionImpl.getFirstInputParamName(functionMicroflow);
+		// FunctionImpl.getFirstInputParamName(functionMicroflow);
+		if (inputParamName == null || inputParamName.isBlank()) {
+			return null;
+		}
+
+		ObjectNode parametersNode = MAPPER.createObjectNode();
+		ObjectNode propertiesNode = MAPPER.createObjectNode();
+		ObjectNode propertyNode = MAPPER.createObjectNode(); 
+		ArrayNode requiredNode = MAPPER.createArrayNode();
+		
+		propertyNode.put("type", "string");
+		
+		propertiesNode.set(inputParamName, propertyNode);
+		
+		requiredNode.add(inputParamName);
+		
+		parametersNode.put("type", "object");
+		parametersNode.set("properties", propertiesNode);
+		parametersNode.set("required", requiredNode);
+		
+		return parametersNode;
+	}
+
+}
