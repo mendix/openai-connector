d8a7dc38212f11e1ac6ac2f6cdabda9b9ada7139
Tina Engel <tina.engel@mendix.com>
2024-06-28 10:20:25 +0000

    SAS-585 creted new JA to ChatContext_Create_SetActionMicroflow ChatContext_AddProviderConfigList got replaced by ChatContext_Create_SetProviderConfigList ChatContext_Create (Create ChatContext) is not exposed anymore and moved to private folder

--- javasource/conversationalui/impl/ProviderConfigImpl.java
+++ javasource/conversationalui/impl/ProviderConfigImpl.java
@@ -1,13 +1,17 @@
 package conversationalui.impl;
 
+import static java.util.Objects.requireNonNull;
+
 import java.util.Map;
 import com.mendix.core.Core;
 import com.mendix.systemwideinterfaces.core.IDataType;
+import com.mendix.systemwideinterfaces.core.IMendixObject;
 import conversationalui.proxies.ChatContext;
+import conversationalui.proxies.ProviderConfig;
 
 public class ProviderConfigImpl{
 	
-	public static void validate(String ActionMicroflow) {
+	public static void validateActionMicroflow(String ActionMicroflow) {
 		if (ActionMicroflow == null || ActionMicroflow.isBlank()) {
 			throw new IllegalArgumentException("ActionMicroflow is required.");
 		}
@@ -24,5 +28,12 @@ public class ProviderConfigImpl{
 		if(Core.getReturnType(ActionMicroflow) == null || IDataType.DataTypeEnum.Boolean.equals(Core.getReturnType(ActionMicroflow).getType()) == false) {
 			throw new IllegalArgumentException("ActionMicroflow " + ActionMicroflow + " should have a Boolean return value.");		
 		}
-	}	
+	}
+	
+	public static void validateSpecialization(IMendixObject ProviderConfigSpecialization) throws Exception {
+		requireNonNull(ProviderConfigSpecialization, "ProviderConfig Specialization is required.");
+		if (!ProviderConfigSpecialization.isInstanceOf(ProviderConfig.entityName)){
+			throw new IllegalArgumentException(ProviderConfig.entityName + " or a specialization of such is required.");
+		}		
+	}
 }
